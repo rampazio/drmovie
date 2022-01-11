b05c02150af7200ef4240fd14f29dcb67e3c3fca
diegorfreitas3009@gmail.com <diegorfreitas3009@gmail.com>
2022-01-11 15:38:58 -0300

    Project Create

---
+++ backend/src/main/java/com/rampazio/drmovie/DrmovieApplication.java
@@ -0,0 +1,13 @@
+package com.rampazio.drmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rampazio/drmovie/DrmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rampazio.drmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
