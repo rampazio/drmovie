8496e334e88a4de4b2a82b73a243a62581563514
rampazio <diegorfreitas3009@gmail.com>
2022-01-15 12:06:45 -0300

    Domain model, database seed

--- backend/src/main/java/com/rampazio/drmovie/config/SecurityConfig.java
+++ backend/src/main/java/com/rampazio/drmovie/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
