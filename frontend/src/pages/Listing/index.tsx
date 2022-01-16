45d3cc0dfa365a98c9b9a38164d796a340964555
rampazio <diegorfreitas3009@gmail.com>
2022-01-16 19:49:51 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,18 +2,28 @@ import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "components/utils/requests";
+import { useEffect } from "react";
+import { useState } from "react";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
-        // FORMA ERRADA
-        axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {console.log(response.data)});
+        const [pageNumber, setPageNumber] = useState(0)
 
+                useEffect(() =>{
+                        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+                        .then(response => {
+                                const data = response.data as MoviePage;
+                                console.log(data);
+                        setPageNumber(data.number)
+                });
+             },[]);
 
 
     return (
 
         <>
+        <p>{pageNumber}</p>
         <Pagination />
 
             <div className="container">
